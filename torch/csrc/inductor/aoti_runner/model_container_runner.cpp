9236526656b6db0c51cd49a47c10c44a38bf663c
Bin Bao <binbao@meta.com>
2023-12-19 06:11:01 -0800

    [AOTI] Refactor model runner API
    Summary: make proxy executor as a private member
    
    ghstack-source-id: ceb9c2c90e03b1053c011d0e1b30b4e020448056
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116047

--- torch/csrc/inductor/aoti_runner/model_container_runner.cpp
+++ torch/csrc/inductor/aoti_runner/model_container_runner.cpp
@@ -7,12 +7,12 @@
 namespace torch::inductor {
 
 AOTIModelContainerRunner::AOTIModelContainerRunner(
-    const char* model_path,
+    const std::string& model_so_path,
     size_t num_models,
     bool is_cpu,
-    const char* cubin_dir) {
-  model_so_ = std::make_unique<at::DynamicLibrary>(model_path);
-  TORCH_CHECK(model_so_, "Failed to load model: ", model_path);
+    const std::string& cubin_dir) {
+  model_so_ = std::make_unique<at::DynamicLibrary>(model_so_path.c_str());
+  TORCH_CHECK(model_so_, "Failed to load model: ", model_so_path);
   create_func_ = reinterpret_cast<decltype(create_func_)>(
       model_so_->sym("AOTInductorModelContainerCreate"));
   delete_func_ = reinterpret_cast<decltype(delete_func_)>(
@@ -34,8 +34,22 @@ AOTIModelContainerRunner::AOTIModelContainerRunner(
   get_call_spec_func_ = reinterpret_cast<decltype(get_call_spec_func_)>(
       model_so_->sym("AOTInductorModelContainerGetCallSpec"));
 
-  AOTI_RUNTIME_ERROR_CODE_CHECK(
-      create_func_(&container_handle_, num_models, is_cpu, cubin_dir));
+  AOTI_RUNTIME_ERROR_CODE_CHECK(create_func_(
+      &container_handle_,
+      num_models,
+      is_cpu,
+      cubin_dir.empty() ? nullptr : cubin_dir.c_str()));
+
+  /*
+  #ifdef FBCODE_CAFFE2
+    std::string extern_kernel_nodes_path =
+        model_so_path.substr(0, model_so_path.size() - 3) + ".json";
+    if (fileExists(extern_kernel_nodes_path)) {
+      proxy_executor_handle_ =
+          std::make_unique<torch::aot_inductor::FbProxyExecutor>(
+              extern_kernel_nodes_path, is_cpu);
+  #endif
+  */
 }
 
 AOTIModelContainerRunner::~AOTIModelContainerRunner() {
@@ -45,9 +59,8 @@ AOTIModelContainerRunner::~AOTIModelContainerRunner() {
 }
 
 std::vector<at::Tensor> AOTIModelContainerRunner::run(
-    std::vector<at::Tensor> inputs,
-    AOTInductorStreamHandle cuda_stream_handle,
-    AOTIProxyExecutorHandle proxy_executor_handle) {
+    std::vector<at::Tensor>& inputs,
+    AOTInductorStreamHandle cuda_stream_handle) {
   auto input_handles =
       torch::aot_inductor::unsafe_alloc_new_handles_from_tensors(inputs);
 
@@ -65,7 +78,7 @@ std::vector<at::Tensor> AOTIModelContainerRunner::run(
       output_handles.data(),
       output_handles.size(),
       cuda_stream_handle,
-      proxy_executor_handle));
+      proxy_executor_handle_));
 
   return torch::aot_inductor::alloc_tensors_by_stealing_from_handles(
       output_handles.data(), output_handles.size());
@@ -92,13 +105,12 @@ void AOTIModelContainerRunner::swap_constant_buffer() {
   AOTI_RUNTIME_ERROR_CODE_CHECK(swap_constant_buffer_func_(container_handle_));
 }
 
-std::vector<const char*> AOTIModelContainerRunner::get_call_spec() {
+std::vector<std::string> AOTIModelContainerRunner::get_call_spec() {
   const char* in_spec;
   const char* out_spec;
   AOTI_RUNTIME_ERROR_CODE_CHECK(
       get_call_spec_func_(container_handle_, &in_spec, &out_spec));
-  std::vector<const char*> call_spec = {in_spec, out_spec};
-  return call_spec;
+  return {in_spec, out_spec};
 }
 
 } // namespace torch::inductor
