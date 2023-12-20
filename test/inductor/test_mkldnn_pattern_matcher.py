fccb1b896dcc85fdfe00f9e21626e63358a5d173
Le-Zheng <le.zheng@intel.com>
2023-12-19 23:32:31 -0800

    rebase [Quant][Inductor] Enable quantization linear pattern fusion with int8_mixed_bf16 for gelu
    ghstack-source-id: 9ea74abbba40cd8de16a2321703940aba0d4ecca
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116004

--- test/inductor/test_mkldnn_pattern_matcher.py
+++ test/inductor/test_mkldnn_pattern_matcher.py
@@ -1161,6 +1161,19 @@ class TestPatternMatcher(TestPatternMatcherBase):
         for gelu in [torch.nn.GELU("none"), torch.nn.GELU("tanh")]:
             self._qlinear_unary_cpu_test_helper(inputs=torch.randn((2, 3, 4)), unary_op=gelu)
 
+
+    @skipIfNoDynamoSupport
+    @skipIfNoONEDNN
+    @skipIfRocm
+    def test_qlinear_gelu_int8_mixed_bf16(self):
+        r"""
+        This testcase will quantize a Linear->GELU pattern with int8_mixed_bf16 quantization.
+        """
+        for gelu in [torch.nn.GELU("none"), torch.nn.GELU("tanh")]:
+            self._qlinear_unary_cpu_test_helper(inputs=torch.randn((2, 3, 4)),
+            unary_op=gelu, int8_mixed_bf16=True
+        )
+
     def _qlinear_dequant_promotion_cpu_test_helper(self, inputs, int8_mixed_bf16=False):
         class M(torch.nn.Module):
             def __init__(
